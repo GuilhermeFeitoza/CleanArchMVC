524029d22e1d239920f4991ddbed2e6219f39ccf
unknown <guilhermefeitoza09@outlook.com>
2024-02-23 12:39:56 -0300

    Enriquecendo modelo de dominio

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,45 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{   //sealed faz com que a classse não possa ser herdada
+   public sealed class Category : Entity
+    {
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id Value");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public void Update(string name) {
+
+            ValidateDomain(name);
+        
+        
+        }
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+
+            DomainExceptionValidation.When(name.Length <3, "Invalid name. Name is too short");
+
+            Name = name;
+        }
+
+      
+     
+
+        public string Name { get; private set; }    
+        public ICollection<Product> Products { get; private set; }
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,69 @@
+﻿using CleanArchMvc.Domain.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public sealed class Product:Entity
+    {
+
+        public string Name { get; private set; } 
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock {  get; private set; }
+
+        public string Image { get; private set; }
+
+        public int CategoryId { get;  set; }
+        public Category Category { get; set; }
+
+
+        private void ValidateDomain(string name, string description, decimal price, int stock , string image)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Name is required");
+
+            DomainExceptionValidation.When(name.Length <3, "Name is too short");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "description is required");
+
+            DomainExceptionValidation.When(description.Length < 3, "description is too short");
+
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
+
+            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, too long");
+
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+
+
+        }
+
+        public Product(string name, string description, decimal price, int stock, string image)
+        {
+            ValidateDomain(name,description,price,stock,image);
+        }
+
+        public Product(int id,string name, string description, decimal price, int stock, string image)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id Value");
+            Id = id;
+            ValidateDomain(name, description, price, stock, image);
+        }
+
+        public void Update(string name, string description, decimal price, int stock, string image, int categoryId)
+        {
+
+            ValidateDomain(name,description,price,stock,image);
+            CategoryId = categoryId;
+
+        }
+    }
+}
