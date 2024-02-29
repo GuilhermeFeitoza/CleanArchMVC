d78b5cf91ed9ca360b15b929847b2e4eba399c5e
Guilherme Feitoza <guilhermefeitoza09@outlook.com>
2024-02-28 21:23:02 -0300

    Migrations adicionadas e

---
+++ CleanArchMvc.Infra.Data/Migrations/20240229001334_SeedProducts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchMvc.Infra.Data.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" +
+                "VALUES('Caderno espiral','100 folhas',7.45,50,'caderno1.jpg',1)");
+            migrationBuilder.Sql("INSERT INTO Products(Name,Description,Price,Stock,Image,CategoryId)" +
+             "VALUES('Lapiseira','0.7',7.45,50,'lapiseira.jpg',1)");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Products");
+        }
+    }
+}
